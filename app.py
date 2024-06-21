071537d72c3357d07a9fa1f686ed5c363bfba2e3
wt <1078210460@qq.com>
2024-06-21 17:15:51 +0800

    所有的都提交一下

--- app.py
+++ app.py
@@ -1,63 +1,11 @@
-from flask import Flask
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy import text
-import sys
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import sql
+import os, sys
+from config.setting import SERVER_PORT
+from api.user import app
 
-HOST = '139.224.60.242'  # 127.0.0.1/localhost
-PORT = 3306
-DATA_BASE = 'some-note'
-USER = 'root'
-PWD = '940327Wt!'
-# DB_URI = f'数据库的名+驱动名://{USER}:{PWD}@{HOST}:{PORT}/{DATA_BASE}'
-DB_URI = f'mysql+pymysql://{USER}:{PWD}@{HOST}:{PORT}/{DATA_BASE}'
-
-app = Flask(__name__)
-engine = create_engine(DB_URI)
-Base = declarative_base(engine)
-session = sessionmaker(engine)()
-
-
-class Note(Base):
-    __tablename__ = 't_note'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    account = Column(String(255))
-    password = Column(Integer)
-
-
-@app.route('/')
-def hello_world():
-    with Session() as session:
-        p2 = Note(account='MySQL', password='SQL')
-        session.add_all([p2])
-        session.commit()
-
-    return 'Hello World!112'
-
+# 项目根路径
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_PATH)  # 将项目根路径临时加入环境变量，程序退出后失效
 
 if __name__ == '__main__':
-    Base.metadata.create_all()
-    app.config.update(DEBUG=True)
-    app.run()
-
-
-@app.route('/users/<user_id>')
-def user_info(user_id):
-    # HOST = '139.224.60.242'  # 127.0.0.1/localhost
-    # PORT = 3306
-    # DATA_BASE = 'some-note'
-    # USER = 'root'
-    # PWD = '940327Wt!'
-    # # DB_URI = f'数据库的名+驱动名://{USER}:{PWD}@{HOST}:{PORT}/{DATA_BASE}'
-    # DB_URI = f'mysql+pymysql://{USER}:{PWD}@{HOST}:{PORT}/{DATA_BASE}'
-    # engine = create_engine(DB_URI)
-    # # 执行一个SQL
-    # sql = 'select * from t_note;'
-    # conn = engine.connect()
-    # rs = conn.execute(text(sql))
-    # print(rs.fetchone())
-
-    print(type(user_id))
-    return 'hello user{}'.format(user_id)
+    # host为主机ip地址，port指定访问端口号，debug=True设置调试模式打开
+    app.run(host="0.0.0.0", port=SERVER_PORT, debug=True)
