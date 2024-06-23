b703f77fa1e1c1f65f32ff386ab0bba5928cf5fe
蜗牛使劲冲 <1078210460@qq.com>
2024-06-23 12:19:01 +0800

    这个可以的，就是代理不行

--- api/film.py
+++ api/film.py
@@ -1,8 +1,13 @@
+from decimal import Decimal
+
 from flask import Flask, jsonify, request
 from common.mysql_operate import db
 from common.redis_operate import redis_db
 from common.md5_operate import get_md5
 import re, time
+import requests
+import json
+
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # jsonify返回的中文正常显示
@@ -25,20 +30,72 @@ def get_all_users():
 
 @app.route("/filmAdd", methods=['POST'])
 def filmAdd():
-
-    title = "大鱼海棠"
-    cover = "https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2361744534.webp"
-    url = "https://movie.douban.com/subject/5045678/"
-    rating = "蒲公英"
-    casts = "\"季冠霖\",\"苏尚卿\""
-    star = "35"
-    directors = "\"张浩\""
-    cover_x = 1080
-    cover_y = 1620
-    other_id = "5045678"
-    sql3 = "INSERT INTO a_film(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id) " \
-           "VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id)
-    print("sql3:" + sql3 )
-    db.execute_db(sql3)
-    print("新增用户信息SQL ==>> {}".format(sql3))
+    # title = "大鱼海棠"
+    # cover = "https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2361744534.webp"
+    # url = "https://movie.douban.com/subject/5045678/"
+    # rating = 6.7
+    # casts = "\"季冠霖\",\"苏尚卿\""
+    # star = "35"
+    # directors = "\"张浩\""
+    # cover_x = 1080
+    # cover_y = 1620
+    # other_id = "5045678"
+    #
+    # sql3 = "INSERT INTO a_film(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id) " \
+    #        "VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id)
+    # print("sql3:" + sql3 )
+    # db.execute_db(sql3)
+    # print("新增用户信息SQL ==>> {}".format(sql3))
     return jsonify({"code": 200, "msg": "ok!"})
+
+@app.route("/collect", methods=['GET'])
+def collect():
+    for i in range(2641, 10000, 20):
+        print("now start is:" + str(i))
+        remoteCollect(i)
+
+    return jsonify({"code": 200, "msg": "collect ok!"})
+
+def remoteCollect(start):
+    url = 'https://movie.douban.com/j/new_search_subjects?sort=T&tags=&start=' + str(start)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+
+            for item in data['data']:
+                title = item['title']
+                cover = item['cover']
+                url = item['url']
+                rating = Decimal(item['rate'])
+                casts = str(item['casts'])
+                star = int(item['star'])
+                directors = str(item['directors'])
+                cover_x = int(item['cover_x'])
+                cover_y = int(item['cover_y'])
+                other_id = str(item['id'])
+                addFilmSql(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id)
+
+
+        except json.decoder.JSONDecodeError:
+            print('Error: Failed to decode JSON data')
+    else:
+        print(f'Error: Failed to fetch data. Status code: {response.status_code}')
+        exit()
+
+def addFilmSql(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id):
+    # sql3 = "INSERT INTO a_film(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id) " \
+    #        "VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(title, cover, url, Decimal(rating),
+    #                                                                                    str(casts), int(star), directors,
+    #                                                                                    int(cover_x), int(cover_y), int(other_id))
+    sql3 = "INSERT INTO a_film(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id) " \
+           "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+    print("title is:" + title)
+    # print("新增用户信息SQL ==>> '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}'".format(title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id))
+    db.execute_db_params(sql3, (title, cover, url, rating, casts, star, directors, cover_x, cover_y, other_id))
+
--- common/mysql_operate.py
+++ common/mysql_operate.py
@@ -46,4 +46,32 @@ class MysqlDb():
             # 回滚所有更改
             self.conn.rollback()
 
+    def execute_db_params(self, sql, params=None):
+        """更新/新增/删除"""
+        try:
+            # 检查连接是否断开，如果断开就进行重连
+            self.conn.ping(reconnect=True)
+
+            # 使用 mogrify 来组合SQL语句和参数
+            compiled_sql = self.cur.mogrify(sql, params)
+            # 打印出完整的SQL语句
+            # print("Executing  SQL: " + compiled_sql)
+            self.addText(compiled_sql, 'film-insert.txt')
+
+            # 使用 execute() 执行sql
+            self.cur.execute(sql, params)
+            # 提交事务
+            self.conn.commit()
+        except Exception as e:
+            print("操作出现错误：{}".format(e))
+            self.addText("错误：{}".format(e), 'film-insert-error.txt')
+            # 回滚所有更改
+            self.conn.rollback()
+
+    def addText(self, content, fileName):
+        with open(fileName, 'a', encoding='utf-8') as file:
+            # 在文件末尾追加文本
+            file.write("\n"+content)
+
+
 db = MysqlDb(MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWD, MYSQL_DB)
